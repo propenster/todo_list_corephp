8a5fd4619902dc1854d02fb7ae502e8692e7c37a
Olusegun Faith Emmanuel <51266654+propenster@users.noreply.github.com>
2020-07-24 18:08:12 +0100

    Updated index.php

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
 ?>
 
 <!DOCTYPE HTML>
-<html lang="ja">
+<html lang="en">
 <head>
     <title>Todo List</title>
     <link rel="stylesheet" href="//fonts.googleapis.com/css?family=Roboto:300,300italic,700,700italic">
